47432a648d3208ff41745a151d757e3de99a43f4
daniel Tadros <daniel.tadros@lenus.io>
2023-10-22 19:55:48 +0200

    Feat: add the fields to the modal

--- apps/frontend/src/components/AddMemberNodeModal.tsx
+++ apps/frontend/src/components/AddMemberNodeModal.tsx
@@ -1,9 +1,12 @@
+import * as React from "react";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
 import DialogTitle from "@mui/material/DialogTitle";
 import { MemberNode } from "shared-types";
+import { MenuItem, OutlinedInput, Select } from "@mui/material";
 import { useForm } from "react-hook-form";
 
 type Props = {
@@ -12,13 +15,36 @@ type Props = {
 };
 
 export const AddMemberNodeModal = ({ onClose, onSave }: Props) => {
-  const { handleSubmit } = useForm<Omit<MemberNode, "id">>();
+  const { handleSubmit, register } =
+    useForm<Omit<MemberNode, "id" | "parentId">>();
 
   return (
     <Dialog open={true} onClose={onClose}>
       <form onSubmit={handleSubmit(onSave)}>
         <DialogTitle>Add a new member</DialogTitle>
-        <DialogContent></DialogContent>
+        <DialogContent>
+          <TextField
+            sx={{ marginTop: 1 }}
+            autoFocus
+            id="name"
+            label="Name"
+            {...register("name", { required: true })}
+            type="text"
+            fullWidth
+            variant="outlined"
+          />
+          <Select
+            sx={{ marginY: 2 }}
+            fullWidth
+            id="type"
+            {...register("type", { required: true })}
+            defaultValue={"Developer"}
+            label="Type"
+          >
+            <MenuItem value={"Developer"}>Developer</MenuItem>
+            <MenuItem value={"Manager"}>Manager</MenuItem>
+          </Select>
+        </DialogContent>
         <DialogActions>
           <Button type="submit">Save</Button>
         </DialogActions>
