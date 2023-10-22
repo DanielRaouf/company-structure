3a155f8c68fd1cd430e6d9dbaee42876b0a6d417
daniel Tadros <daniel.tadros@lenus.io>
2023-10-22 19:38:30 +0200

    Feat: add fetch logic to the hook "useMemberNode"

--- apps/frontend/src/hooks/useMemberNode.ts
+++ apps/frontend/src/hooks/useMemberNode.ts
@@ -1,10 +1,21 @@
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { MemberNode } from "shared-types";
+
+const baseUrl = process.env.BASE_URL ?? "http://localhost:8080";
 
 export const useMemberNode = (id: number) => {
   const [children, setChildren] = useState<MemberNode[]>([]);
 
   const addNode = useCallback(async (node: Omit<MemberNode, "id">) => {}, []);
+
+  useEffect(() => {
+    const fetchChildren = async () => {
+      const res = await fetch(`${baseUrl}/children/${id}`);
+      const nodes: MemberNode[] = await res.json();
+      setChildren(nodes);
+    };
+    fetchChildren();
+  }, [id, setChildren]);
 
   return { children, addNode };
 };
