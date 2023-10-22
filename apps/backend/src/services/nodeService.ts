b1d496dc0da35703191b2c8b493b7dc23a7b4b11
daniel Tadros <daniel.tadros@lenus.io>
2023-10-22 18:32:32 +0200

    Feat: add the basic three methods to the nodeService

--- apps/backend/src/services/nodeService.ts
+++ apps/backend/src/services/nodeService.ts
@@ -1,4 +1,36 @@
+import { FastifyRequest } from "fastify";
 import { MemberNode } from "shared-types";
 
 // TODO: replace by a real database
 const memberNodes: MemberNode[] = [{ id: 1, name: "Keld", type: "Manager" }];
+
+export const getNodeChildren = async (
+  request: FastifyRequest<{ Params: { id: number } }>
+) => {
+  return memberNodes.filter(
+    (node) => node.parentId === Number(request.params.id)
+  );
+};
+
+export const addNode = async (
+  request: FastifyRequest<{ Body: Omit<MemberNode, "id"> }>
+) => {
+  const id = Math.max(...memberNodes.map((node) => node.id)) + 1;
+  const node = { id, ...request.body };
+  memberNodes.push(node);
+  return node;
+};
+
+export const updateNode = async (
+  request: FastifyRequest<{
+    Params: { id: number };
+    Body: { parentId: number };
+  }>
+) => {
+  const node = memberNodes.find((node) => node.id === request.params.id);
+  if (!node) {
+    throw new Error("Node couldn't be found");
+  }
+  node.parentId = request.body.parentId;
+  return node;
+};
