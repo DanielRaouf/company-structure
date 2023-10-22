dfbb62f2dee324f6506bbf955365171bace326aa
daniel Tadros <daniel.tadros@lenus.io>
2023-10-22 19:42:38 +0200

    Feat: add child nodes rendering

--- apps/frontend/src/components/MemberNode.tsx
+++ apps/frontend/src/components/MemberNode.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Paper, Typography } from "@mui/material";
+import { Box, Paper, Typography } from "@mui/material";
 import { MemberNode } from "shared-types";
 import { useMemberNode } from "../hooks/useMemberNode";
 
@@ -11,29 +11,46 @@ export const MemberNodeComponent: FC<Props> = ({ node }) => {
   const { id, name, parentId, type } = node;
   const { children, addNode } = useMemberNode(id);
   return (
-    <Paper
-      elevation={3}
-      style={{
-        padding: "2px 15px",
-        display: "flex",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Typography variant="h6">Node Information</Typography>
-      <Typography variant="body1">Node ID: {id}</Typography>
-      <Typography variant="body1">Node Name: {name}</Typography>
-      <Typography variant="body1">Parent Node: {parentId}</Typography>
-      {type === "Manager" && (
-        <Typography variant="body1">
-          Department Managed: {node.departmentName}
-        </Typography>
-      )}
-      {type === "Developer" && (
-        <Typography variant="body1">
-          Preferred Language: {node.programingLanguage}
-        </Typography>
-      )}
-    </Paper>
+    <Box>
+      <Paper
+        elevation={3}
+        style={{
+          padding: "2px 15px",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Typography variant="h6">Node Information</Typography>
+        <Typography variant="body1">Node ID: {id}</Typography>
+        <Typography variant="body1">Node Name: {name}</Typography>
+        <Typography variant="body1">Parent Node: {parentId}</Typography>
+        {type === "Manager" && (
+          <Typography variant="body1">
+            Department Managed: {node.departmentName}
+          </Typography>
+        )}
+        {type === "Developer" && (
+          <Typography variant="body1">
+            Preferred Language: {node.programingLanguage}
+          </Typography>
+        )}
+      </Paper>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        {children.map((childNode) => (
+          <Box>
+            <MemberNodeComponent
+              node={{ ...childNode, height: node.height + 1 }}
+            />
+          </Box>
+        ))}
+      </Box>
+    </Box>
   );
 };
