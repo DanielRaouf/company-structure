18cf232babc7922fcf3f36f25794f9d4cd1490e9
daniel Tadros <daniel.tadros@lenus.io>
2023-10-22 19:05:44 +0200

    Test: add new test for update node

--- apps/backend/src/services/memberNodeService.test.ts
+++ apps/backend/src/services/memberNodeService.test.ts
@@ -5,7 +5,7 @@ import * as memberNodeService from "./memberNodeService";
 const TEST_NAME = "name1";
 
 describe("memberNodeService test", () => {
-  it("create a member", async () => {
+  it("addNode should create a new member", async () => {
     const addedNode = await memberNodeService.addNode({
       body: {
         name: TEST_NAME,
@@ -58,5 +58,39 @@ describe("memberNodeService test", () => {
     } as FastifyRequest<{ Params: { id: number } }>);
     expect(childNodes).toHaveLength(1);
     expect(childNodes.at(0)?.parentId).toBeUndefined();
+  });
+
+  it("updateNode should update the node parentId", async () => {
+    const addedNode = await memberNodeService.addNode({
+      body: {
+        name: TEST_NAME,
+        type: "Developer",
+        parentId: 1,
+      },
+    } as FastifyRequest<{ Body: Omit<MemberNode, "id"> }>);
+
+    const addedNodeAsNewParent = await memberNodeService.addNode({
+      body: {
+        name: TEST_NAME,
+        type: "Developer",
+        parentId: 1,
+      },
+    } as FastifyRequest<{ Body: Omit<MemberNode, "id"> }>);
+    await memberNodeService.updateNode({
+      params: { id: addedNode.id },
+      body: {
+        parentId: addedNodeAsNewParent.id,
+      },
+    } as FastifyRequest<{
+      Params: { id: number };
+      Body: { parentId: number };
+    }>);
+    const childNodes = await memberNodeService.getNodeChildren({
+      params: {
+        id: addedNodeAsNewParent.id,
+      },
+    } as FastifyRequest<{ Params: { id: number } }>);
+    expect(childNodes).toHaveLength(1);
+    expect(childNodes.at(0)?.id).toEqual(addedNode.id);
   });
 });
